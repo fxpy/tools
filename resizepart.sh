c3cfcc3cb5e76aa38f4e8e777d1b9e1694baf0cb
fxpy <58055522+fxpy@users.noreply.github.com>
2021-11-05 14:41:23 +0300

    Create resizepart.sh
    init

---
+++ resizepart.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
+if exists growpart; then
+        echo ''
+else
+  sudo apt install growpart -y
+fi
+
+echo '-=-=-=-=-=-=-=-=-=-=-'
+echo "Auto expand disk size"
+echo '-=-=-=-=-=-=-=-=-=-=-'
+
+df -h
+echo '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+echo "Which partition you want to expand? Enter /dev/sda NUMBER." 
+read -p "Example: for /dev/sda2 enter 2: " PART_NUM
+echo '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+
+growpart /dev/sda $PART_NUM
+resize2fs /dev/sda$PART_NUM
+
+echo '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+echo "Your partition /dev/sda"$PART_NUM "resized"
+echo '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+
+df -h
